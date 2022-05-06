c249aca757fdd808642840b77d6f362977ad0ebf
aherredev <aherredev@gmail.com>
2022-05-06 13:13:47 -0500

    Parse schema into dart file

---
+++ lib/graphql_types_builder.dart
@@ -0,0 +1,6 @@
+library graphql_types_builder;
+
+import 'package:build/build.dart';
+import 'package:graphql_types_builder/src/graphql_types_builder.dart';
+
+Builder graphqlTypesBuilder(BuilderOptions options) => GraphqlTypesBuilder();
